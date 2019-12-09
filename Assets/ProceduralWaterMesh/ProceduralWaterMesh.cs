58af4b67f9b6d5987ce539a081a37fba801d546c
Aras Pranckevicius <aras@unity3d.com>
2019-12-09 13:31:43 +0200

    Cleanup and optimize

--- Assets/ProceduralWaterMesh/ProceduralWaterMesh.cs
+++ Assets/ProceduralWaterMesh/ProceduralWaterMesh.cs
@@ -1,11 +1,10 @@
-﻿using System.Collections.Generic;
-using System.Linq;
+﻿using System.Linq;
 using UnityEngine;
 
 // Simple water wave procedural mesh based on http://www.konsfik.com/procedural-water-surface-made-in-unity3d/ - written by Kostas Sfikas, March 2017.
 //
-// "Classic API" with 200x200 vertex mesh, 4 wave sources, on 2018 MacBookPro (Core i9 2.9GHz):
-// - 42.6ms, 0.7MB GC alloc
+// "Classic API" with 250x250 vertex mesh, 4 wave sources, on 2018 MacBookPro (Core i9 2.9GHz):
+// - 38.0ms, no GC allocations
 [RequireComponent(typeof(MeshFilter))]
 [RequireComponent(typeof(MeshRenderer))]
 public class ProceduralWaterMesh : MonoBehaviour
@@ -14,7 +13,8 @@ public class ProceduralWaterMesh : MonoBehaviour
 	public float surfaceActualLength = 10;
 	public int surfaceWidthPoints = 100;
 	public int surfaceLengthPoints = 100;
-	List<Transform> waveSources;
+	Transform[] waveSources;
+	Vector3[] vertices;
 	float localTime;
 	float localTimeScale;
 
@@ -22,7 +22,7 @@ public class ProceduralWaterMesh : MonoBehaviour
 	{
 		localTime = 0.0f;
 		CreateMesh ();
-		waveSources = transform.Cast<Transform>().Where(t => t.gameObject.activeInHierarchy).ToList();
+		waveSources = transform.Cast<Transform>().Where(t => t.gameObject.activeInHierarchy).ToArray();
 	}
 
 	// Update is called once per frame
@@ -34,61 +34,62 @@ public class ProceduralWaterMesh : MonoBehaviour
 	void UpdateWaterMesh()
 	{
 		Mesh waterMesh = GetComponent<MeshFilter>().mesh;
-		var verts = waterMesh.vertices;
+		var verts = vertices;
 		for (int i = 0; i < verts.Length; i++)
-		{
-			float x = verts [i].x;
-			float y = RecalculatePointY(verts[i]);
-			float z = verts [i].z;
-			Vector3 p = new Vector3(x,y,z);
-			verts[i] = p;
-		}
+			verts[i] = RecalculatePointY(verts[i]);
 		waterMesh.vertices = verts;
 		waterMesh.RecalculateNormals();
 	}
 
-	float RecalculatePointY(Vector3 point)
+	Vector3 RecalculatePointY(Vector3 p)
 	{
 		var y = 0.0f;
-		for (var i = 0; i < waveSources.Count; i++)
+		for (var i = 0; i < waveSources.Length; i++)
 		{
-			var p1 = new Vector2 (point.x, point.z);
+			var p1 = new Vector2 (p.x, p.z);
 			var p2 = new Vector2 (waveSources[i].position.x, waveSources[i].position.z);
 			var dist = Vector2.Distance (p1,p2);
 			y += Mathf.Sin (dist * 12.0f - localTime) / (dist*20+10);
 		}
-		return y;
+		p.y = y;
+		return p;
 	}
 
 	void CreateMesh()
 	{
 		Mesh newMesh = new Mesh();
-		List<Vector3> verticeList = new List<Vector3>();
-		List<int> triList = new List<int>();
-		for (int i = 0; i < surfaceWidthPoints; i++)
+		vertices = new Vector3[surfaceWidthPoints * surfaceLengthPoints];
+		var indices = new int[(surfaceWidthPoints - 1) * (surfaceLengthPoints - 1) * 6];
+		var index = 0;
+		for (var i = 0; i < surfaceWidthPoints; i++)
 		{		
-			for (int j = 0; j < surfaceLengthPoints; j++)
+			for (var j = 0; j < surfaceLengthPoints; j++)
 			{
-				float x = MapValue (i, 0.0f, surfaceWidthPoints, -surfaceActualWidth/2.0f, surfaceActualWidth/2.0f);
-				float z = MapValue (j, 0.0f, surfaceLengthPoints, -surfaceActualLength/2.0f, surfaceActualLength/2.0f);
-				verticeList.Add(new Vector3(x, 0f, z));
-				//Skip if a new square on the plane hasn't been formed
-				if (i == 0 || j == 0)
-					continue;
-				//Adds the index of the three vertices in order to make up each of the two tris
-				triList.Add(surfaceLengthPoints * i +j); //Top right
-				triList.Add(surfaceLengthPoints * i + j - 1); //Bottom right
-				triList.Add(surfaceLengthPoints * (i - 1) + j - 1); //Bottom left - First triangle
-				triList.Add(surfaceLengthPoints * (i - 1) + j - 1); //Bottom left 
-				triList.Add(surfaceLengthPoints * (i- 1) + j); //Top left
-				triList.Add(surfaceLengthPoints * i + j); //Top right - Second triangle
+				float x = MapValue (i, 0.0f, surfaceWidthPoints-1, -surfaceActualWidth/2.0f, surfaceActualWidth/2.0f);
+				float z = MapValue (j, 0.0f, surfaceLengthPoints-1, -surfaceActualLength/2.0f, surfaceActualLength/2.0f);
+				vertices[index++] = new Vector3(x, 0f, z);
 			}
 		}
-		//creating the mesh with the data generated above
-		newMesh.vertices = verticeList.ToArray();	//pass vertices to mesh
-		newMesh.triangles = triList.ToArray();		//pass triabgles to mesh
-		newMesh.RecalculateNormals();				//recalculate mesh normals
-		GetComponent<MeshFilter>().mesh = newMesh;	//pass the created mesh to the mesh filter
+
+		index = 0;
+		for (var i = 0; i < surfaceWidthPoints-1; i++)
+		{		
+			for (var j = 0; j < surfaceLengthPoints-1; j++)
+			{
+				var baseIndex = i * surfaceLengthPoints + j;
+				indices[index++] = baseIndex;
+				indices[index++] = baseIndex + 1;
+				indices[index++] = baseIndex + surfaceLengthPoints + 1;
+				indices[index++] = baseIndex; 
+				indices[index++] = baseIndex + surfaceLengthPoints + 1;
+				indices[index++] = baseIndex + surfaceLengthPoints;
+			}
+		}
+		
+		newMesh.vertices = vertices;
+		newMesh.triangles = indices;
+		newMesh.RecalculateNormals();
+		GetComponent<MeshFilter>().mesh = newMesh;
 	}
 
 	float MapValue(float refValue, float refMin, float refMax, float targetMin, float targetMax)
