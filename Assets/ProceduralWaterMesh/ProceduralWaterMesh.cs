63fc2972c5ec6e03d555feb3b2aa6b19031c3f52
Aras Pranckevicius <aras@unity3d.com>
2021-04-07 17:55:28 +0300

    Water scene: add GPU path, options UI, etc

--- Assets/ProceduralWaterMesh/ProceduralWaterMesh.cs
+++ Assets/ProceduralWaterMesh/ProceduralWaterMesh.cs
@@ -3,31 +3,44 @@ using Unity.Burst;
 using Unity.Collections;
 using Unity.Jobs;
 using UnityEngine;
+using UnityEngine.Rendering;
 
-// Simple water wave procedural mesh based on http://www.konsfik.com/procedural-water-surface-made-in-unity3d/ - written by Kostas Sfikas, March 2017.
+// Simple water wave procedural mesh based on https://www.konsfik.com/procedural-water-surface-made-in-unity3d/ - written by Kostas Sfikas, March 2017.
 //
-// Tests on 250x250 vertex mesh, 10 wave sources, on 2018 MacBookPro (Core i9 2.9GHz):
-// "Classic API":
-// - 66.0ms, no GC allocations
-//
-// Jobs without Burst:
-// - 11.2ms (9.6ms job, 1.4ms RecalcNormals, some unaccounted)
-// Jobs with Burst:
-// - 3.9ms (2.4ms job, 1.4ms RecalcNormals, some unaccounted)
+// Tests on 400x400 vertex mesh, 10 wave sources, on 2019 MacBookPro (Core i9 2.4GHz, 8c/16t):
+// In editor, full frame time:
+// - C# 1 thread: 154.9ms
+// - Burst 1 thread: 40.2ms
+// - Burst threads: 8.8ms
+// - GPU compute: 8.3ms
 [RequireComponent(typeof(MeshFilter))]
 [RequireComponent(typeof(MeshRenderer))]
 public class ProceduralWaterMesh : MonoBehaviour
 {
-	public bool useJobs = false;
+	public enum Mode
+	{
+		CPU,
+		CPUBurst,
+		CPUBurstThreaded,
+		GPU
+	}
+
+	public Mode mode = Mode.CPUBurstThreaded;
 	public float surfaceActualWidth = 10;
 	public float surfaceActualLength = 10;
 	public int surfaceWidthPoints = 100;
 	public int surfaceLengthPoints = 100;
+	public ComputeShader waveComputeShader;
 	Transform[] m_WaveSources;
-	NativeArray<Vector3> m_WaveSourcePositions;
-	NativeArray<Vector3> m_Vertices;
 	Mesh m_Mesh;
 	float m_LocalTime = 0.0f;
+
+	NativeArray<Vector3> m_WaveSourcePositions;
+	NativeArray<Vector3> m_Vertices;
+	GraphicsBuffer m_GpuWaveSourcePositions;
+	GraphicsBuffer m_GpuVertices;
+	
+	GUIContent[] m_UIOptions;
 
 	void OnEnable()
 	{
@@ -40,19 +53,43 @@ public class ProceduralWaterMesh : MonoBehaviour
 	{
 		m_WaveSourcePositions.Dispose();
 		m_Vertices.Dispose();
+		CleanupComputeResources();
 	}
 
-	void Update()
+	void CleanupComputeResources()
+	{
+		m_GpuWaveSourcePositions?.Dispose();
+		m_GpuWaveSourcePositions = null;
+		m_GpuVertices?.Dispose();
+		m_GpuVertices = null;
+	}
+
+	public void Update()
 	{
 		m_LocalTime += Time.deltaTime * 2.0f;
 		UpdateWaveSourcePositions();
+		if (mode == Mode.GPU)
+			UpdateWaveGpu();
+		else
+			UpdateWaveCpu();
+	}
+
+	void UpdateWaveCpu()
+	{
+		CleanupComputeResources();
+
 		var job = new WaveJob { vertices = m_Vertices, waveSourcePositions = m_WaveSourcePositions, time = m_LocalTime };
-		if (!useJobs)
+		
+		if (mode == Mode.CPU)
 		{
-			for (int i = 0; i < m_Vertices.Length; i++)
+			for (var i = 0; i < m_Vertices.Length; ++i)
 				job.Execute(i);
 		}
-		else
+		else if (mode == Mode.CPUBurst)
+		{
+			job.Schedule(m_Vertices.Length, m_Vertices.Length).Complete();
+		}
+		else if (mode == Mode.CPUBurstThreaded)
 		{
 			job.Schedule(m_Vertices.Length, 16).Complete();
 		}
@@ -60,12 +97,31 @@ public class ProceduralWaterMesh : MonoBehaviour
 		m_Mesh.RecalculateNormals();
 	}
 
+	void UpdateWaveGpu()
+	{
+		m_GpuWaveSourcePositions ??= new GraphicsBuffer(GraphicsBuffer.Target.Structured, m_WaveSources.Length, 12);
+		m_GpuVertices ??= m_Mesh.GetVertexBuffer(0);
+		
+		m_GpuWaveSourcePositions.SetData(m_WaveSourcePositions);
+		
+		waveComputeShader.SetFloat("gTime", m_LocalTime);
+		waveComputeShader.SetInt("gVertexCount", m_Mesh.vertexCount);
+		waveComputeShader.SetInt("gWaveSourceCount", m_WaveSources.Length);
+		waveComputeShader.SetInt("gVertexGridX", surfaceWidthPoints);
+		waveComputeShader.SetInt("gVertexGridY", surfaceLengthPoints);
+		// update vertex positions
+		waveComputeShader.SetBuffer(0, "bufVertices", m_GpuVertices);
+		waveComputeShader.SetBuffer(0, "bufWaveSourcePositions", m_GpuWaveSourcePositions);
+		waveComputeShader.Dispatch(0, (m_Mesh.vertexCount+63)/63, 1, 1);
+		// calculate normals
+		waveComputeShader.SetBuffer(1, "bufVertices", m_GpuVertices);
+		waveComputeShader.Dispatch(1, (m_Mesh.vertexCount+63)/63, 1, 1);
+	}
+
 	void UpdateWaveSourcePositions()
 	{
 		for (var i = 0; i < m_WaveSources.Length; ++i)
-		{
 			m_WaveSourcePositions[i] = m_WaveSources[i].position;
-		}
 	}
 
 	[BurstCompile]
@@ -94,6 +150,8 @@ public class ProceduralWaterMesh : MonoBehaviour
 	Mesh CreateMesh()
 	{
 		Mesh newMesh = new Mesh();
+		newMesh.indexFormat = IndexFormat.UInt32;
+		newMesh.vertexBufferTarget |= GraphicsBuffer.Target.Raw;
 		m_Vertices = new NativeArray<Vector3>(surfaceWidthPoints * surfaceLengthPoints, Allocator.Persistent, NativeArrayOptions.UninitializedMemory);
 		var indices = new int[(surfaceWidthPoints - 1) * (surfaceLengthPoints - 1) * 6];
 		var index = 0;
@@ -134,4 +192,20 @@ public class ProceduralWaterMesh : MonoBehaviour
 	{
 		return targetMin + (refValue - refMin) * (targetMax - targetMin) / (refMax - refMin);
 	}
+	
+	public void OnGUI()
+	{
+		m_UIOptions ??= new[]
+		{
+			new GUIContent("C# 1 thread"),
+			new GUIContent("Burst 1 thread"),
+			new GUIContent("Burst threaded"),
+			new GUIContent("GPU compute"),
+		};
+		GUI.matrix = Matrix4x4.Scale(Vector3.one * 2);
+		GUILayout.BeginArea(new Rect(5,25,420,80), "Options", GUI.skin.window);
+		mode = (Mode)GUILayout.Toolbar((int)mode, m_UIOptions);
+		GUILayout.Label($"Water: {surfaceWidthPoints}x{surfaceLengthPoints}, {m_WaveSources.Length} wave sources");
+		GUILayout.EndArea();
+	}	
 }
